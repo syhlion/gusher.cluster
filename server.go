c5c998ab86475b579c2a444fa8df1b42b68eb0b6
Scott <syhlion@gmail.com>
2016-10-07 11:05:50 +0800

    fix mesaage read limit

--- server.go
+++ server.go
@@ -95,6 +95,7 @@ func slave(c *cli.Context) {
 	rsHub := redisocket.NewHub(rpool)
 	rsHub.Config.Upgrader.WriteBufferSize = 8192
 	rsHub.Config.Upgrader.ReadBufferSize = 8192
+	rsHub.Config.MaxMessageSize = 4096
 	rsHubErr := make(chan error, 1)
 	go func() {
 		rsHubErr <- rsHub.Listen()
